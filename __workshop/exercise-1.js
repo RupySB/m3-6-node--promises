9b27e08761a42e94782722a4d4bb9c3bb1c55999
Rupinder Bagri <RBagri94@hotmail.com>
2020-07-10 16:15:00 -0400

    Q1

--- __workshop/exercise-1.js
+++ __workshop/exercise-1.js
@@ -1,20 +1,41 @@
 // Exercise 1
 // ------------
-const arrayOfWords = ['cucumber', 'tomatos', 'avocado'];
-const complicatedArray = ['cucumber', 44, true];
+const arrayOfWords = ["cucumber", "tomatos", "avocado"];
+const complicatedArray = ["cucumber", 44, true];
 
 const makeAllCaps = (array) => {
-  // write some code
+  let myPromise = new Promise((resolve, reject) => {
+    let string = array.every((element) => {
+      return typeof element === "string";
+    });
+
+    if (string) {
+      resolve(
+        arrayOfWords.map(function (x) {
+          return x.toUpperCase();
+        })
+      );
+    } else {
+      reject("This array consists of more than strings");
+    }
+  });
+  return myPromise;
 };
 
 const sortWords = (array) => {
+  //you write array here since its the result of the previous function
+  console.log(array);
+  let myPromise = new Promise((resolve, reject) => {
+    resolve(array.sort());
+  });
+  return myPromise;
   // write some code
 };
 
 // Calling (testing)
 makeAllCaps(arrayOfWords)
   .then(sortWords)
-  .then((result) => console.log(result))
+  .then((result) => console.log(result, "string"))
   .catch((error) => console.log(error));
 
 makeAllCaps(complicatedArray)
