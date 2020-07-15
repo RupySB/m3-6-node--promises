9bb64845513a69f686e9eb2f01aa3dd49361df38
Rupinder Bagri <RBagri94@hotmail.com>
2020-07-15 14:14:19 -0400

    Q2.3

--- __workshop/exercise-2.3.js
+++ __workshop/exercise-2.3.js
@@ -2,6 +2,21 @@
 // ----------------------------
 
 // require the 'request-promise' module.
-const request = require('request-promise');
+const request = require("request-promise");
 
 // get the code you wrote in 2.2 and paste it here...
+const greeting = (langCode) => {
+  return request("https://journeyedu.herokuapp.com/hello/fr") // 1
+    .then((response) => JSON.parse(response))
+    .then((parsedResponse) => {
+      return parsedResponse.data.text; // 2
+    })
+    .catch((err) => {
+      return err.error ? JSON.parse(err.error) : err;
+    });
+};
+
+// Testing
+greeting("fr").then((result) => console.log(result)); // { lang: "French", code: "FR", text: "Bonjour" }
+
+// 3
