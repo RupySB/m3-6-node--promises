aaa859826ca96d65dcf79cd4cc2d5af905d384e2
Rupinder Bagri <RBagri94@hotmail.com>
2020-07-15 14:14:24 -0400

    Q3.1

--- __workshop/exercise-3.1.js
+++ __workshop/exercise-3.1.js
@@ -1,9 +1,19 @@
 // Exercise 3.1 - `getIssPosition`
 // -----------------------------
 
-// require the 'request-promise' module.
-
+// trial 1
+const request = require("request-promise");
 // Returns the current position of the ISS
-const getIssPosition = () => {
-  // write some code...
+const getIssPosition = (lat, lng) => {
+  return request("http://api.open-notify.org/iss-now.json")
+    .then((response) => JSON.parse(response))
+    .then((parsedResponse) => {
+      return {
+        lng: parsedResponse.iss_position.longitude,
+        lat: parsedResponse.iss_position.latitude,
+      };
+    });
 };
+
+// write some code..
+getIssPosition("").then((result) => console.log(result));
