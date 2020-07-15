622d499f18cacd3651a9a3cb428f5dba3a2d10a2
Rupinder Bagri <RBagri94@hotmail.com>
2020-07-15 14:14:31 -0400

    Q3.2

--- __workshop/exercise-3.2.js
+++ __workshop/exercise-3.2.js
@@ -1,18 +1,20 @@
 // Exercise 3.2 - `getAddressPosition`
 // ---------------------------------
 
-const opencage = require('opencage-api-client');
-require('dotenv').config();
+const opencage = require("opencage-api-client");
+require("dotenv").config();
+console.log(process.env["OPENCAGE_API_KEY"]);
 
 const getPositionFromAddress = (address) => {
-  const requestObj = {
-    key: '<MY_API_KEY>',
-    q: '<QUERY_STRING>',
+  const openCageObj = {
+    key: process.env.OPENCAGE_API_KEY,
+    q: address,
   };
-
-  // return something...
+  return opencage.geocode(openCageObj).then((response) => {
+    return response.results[0].geometry; //remove.geometry or results to get it
+  });
 };
 
 getPositionFromAddress(
-  '1455 Boulevard de Maisonneuve O, Montréal, QC H3G 1M8'
+  "1455 Boulevard de Maisonneuve O, Montréal, QC H3G 1M8"
 ).then((response) => console.log(response));
